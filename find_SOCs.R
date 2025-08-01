c3d7d778c71bd964d8de1181fcbe3b89dbd4ec14
John Everett <everett.jk@gmail.com>
2025-08-01 01:25:20 +0000

    first commit

---
+++ find_SOCs.R
@@ -0,0 +1,155 @@
+library(dplyr)
+library(data.table)
+library(parallel)
+library(GenomicRanges)
+source('lib.R')
+
+args <- commandArgs(trailingOnly=TRUE)
+
+expSites <- args[1]
+refSites <- args[2]
+
+expSites <- 'p12418-13.tsv.gz'
+refSites <- 'safetyPaperIntSites.tsv.gz'
+
+CPUs <- 30
+minSitesPerPatientChromosome <- 50
+window_width <- 10000
+window_step <- 5000
+n_draws <- 10000
+min_eval <- 0.001
+min_SitesPerCluster <- 5
+chromosomeLengths <- 'hg38_chromosome_lengths.tsv'
+outputFile <- 'output.tsv'
+
+refSites <- readr::read_tsv(refSites, show_col_types = FALSE) %>% as.data.table()
+expSites <- prepIntSites(readr::read_tsv(expSites, show_col_types = FALSE))
+expSites <- mutate(expSites, k = paste0(trial, '~', patient, '~', seqnames, '~', strand)) %>% as.data.table()
+chromosomeLengths <- readr::read_tsv(chromosomeLengths, show_col_types = FALSE) %>% as.data.table()
+
+expSites <- rbindlist(lapply(split(expSites, expSites$k), function(x){
+              if(n_distinct(x$posid) < minSitesPerPatientChromosome) x <- data.table()
+              x
+            }))
+
+windows <- rbindlist(lapply(split(chromosomeLengths, 1:nrow(chromosomeLengths)), function(x){
+             start <- seq(1, x$length, window_step)
+             stop <- seq(window_step, x$length, window_step)
+             data.table(chromosome = x$chromosome, start = start[1:(length(start)-1)], stop = stop)
+            }))
+
+refSites <- subset(refSites, seqnames %in% windows$chromosome)
+expSites <- subset(expSites, seqnames %in% windows$chromosome)
+
+cluster <- parallel::makeCluster(CPUs)
+
+clusterExport(cluster, c('refSites', 'windows', 'minSitesPerPatientChromosome', 'n_draws', 'min_SitesPerCluster'))
+
+worker <- function(x, useFocusedWindows = FALSE){
+       library(dplyr)
+       library(data.table)
+       library(GenomicRanges)
+    
+       # Limit windows to those overlapping with query sites.
+       q <- makeGRangesFromDataFrame(x)
+       
+       if(useFocusedWindows){
+         if(x$k[1] %in% names(focusedWindows)){
+           w <- focusedWindows[[x$k[1]]]
+         } else {
+           return(data.table())
+         }
+       } else {
+         chromosome_windows <- windows[chromosome == x$seqnames[1]]
+         w <- makeGRangesFromDataFrame(chromosome_windows)
+         chromosome_windows <- chromosome_windows[unique(subjectHits(findOverlaps(q, w)))]
+         w <- makeGRangesFromDataFrame(chromosome_windows)
+       }
+  
+       # Create a set of comp sites for this chromosome, same orientation, excluding this patient.
+       setkey(refSites, seqnames, strand, patient)
+       compSites <- refSites[seqnames == x$seqnames[1] & strand == x$strand[1] & patient != x$patient[1]]
+
+       o <- findOverlaps(q, w, ignore.strand = TRUE)
+       m <- as.data.table(w[subjectHits(o)])[, c(-4,-5)]
+       q_tab <- cbind(m[, .(count1 = .N), by = .(seqnames, start, end)])
+       
+       s_tab <- rbindlist(lapply(1:n_draws, function(draw){
+                  set.seed(draw)
+                  z <- makeGRangesFromDataFrame(compSites[sample(.N, n_distinct(x$posid))])
+                  o <- findOverlaps(z, w, ignore.strand = TRUE)
+                  m <- as.data.table(w[subjectHits(o)])[, c(-4,-5)]
+                  cbind(draw  = draw, m[, .(count2 = .N), by = .(seqnames, start, end)])
+               }))
+       
+       s_tab <- s_tab[complete.cases(s_tab)]
+       
+       setkey(s_tab, seqnames, start, end)
+       
+       j <- s_tab[q_tab, on = .(seqnames, start, end)]
+       
+       result <- j[, .(
+         count = count1[1],
+         sims = n_draws,
+         numSimsMatching = sum(count2 >= count1[1]),
+         pVal = (sum(count2 >= count1[1]) + 1) / (n_draws + 1)
+       ), by = .(seqnames, start, end)]
+       
+       result <- result[count >= min_SitesPerCluster]
+       
+       if(nrow(result) > 0){
+         return(cbind(k = x$k[1], result))
+       } else {
+         return(data.table())
+       }
+}
+
+
+r <- rbindlist(parLapply(cluster, split(expSites, expSites$k), worker, useFocusedWindows = FALSE))
+  
+
+if(nrow(r) > 0){
+    focusedWindows <- lapply(split(r, r$k), function(x){
+      x <- subset(x, pVal <= min_eval & count >= min_SitesPerCluster)
+  
+      if(nrow(x) > 0){
+        if(nrow(x) > 1){
+          return(GenomicRanges::reduce(makeGRangesFromDataFrame(x)))
+        } else {
+          return(makeGRangesFromDataFrame(x))
+        }
+      } else {
+        return(GRanges())
+      }
+    })
+
+    clusterExport(cluster, c('focusedWindows'))
+
+    expSites <- subset(expSites, k %in% names(focusedWindows))
+
+    r2 <- rbindlist(parLapply(cluster, split(expSites, expSites$k), worker, useFocusedWindows = TRUE))
+} else {
+    r2 <- data.table()
+}
+  
+if(nrow(r2) > 0){
+    r2$range <- paste(round((r2$end - r2$start + 1) / 1000, 1), 'KB')
+    r2 <- r2[complete.cases(r2)]
+    r2 <- tidyr::separate(r2, k, c('trial', 'patient', 'chromosome', 'strand'), sep = '~')
+}
+  
+parallel::stopCluster(cluster)
+
+readr::write_tsv(r2, outputFile)
+
+# Set hgt.customText to URL serving one of the bed files created below.
+# http://genome.ucsc.edu/cgi-bin/hgTracks?db=hg38&hgt.customText=http://bushmanlab.org/tracks/Boston_SCID2_p00009.bed
+
+invisible(lapply(split(expSites, paste(expSites$trial, expSites$patient)), function(x){
+  createIntUCSCTrack(as.data.frame(x), 
+                     title = paste0(x$trial[1], '_', x$patient[1], '_SOCS'), 
+                     siteLabel = 'siteLabel', 
+                     outputFile = paste0(x$trial[1], '_', x$patient[1], '_SOCS.bed'))
+  
+  system(paste('gzip -9', paste0(x$trial[1], '_', x$patient[1], '_SOCS.bed')))
+}))
---
+++ lib.R
@@ -0,0 +1,103 @@
+
+
+prepIntSites <- function(x){
+  
+  if(! 'timePointMonths' %in% names(x)) x <- bind_cols(x, expandTimePoints(x$timePoint))
+  
+  x <- subset(x, timePointDays >= minTimePointDays)
+  if(nrow(x) == 0) stop('No sites remain after filtering on minTimePointDays')
+
+  if(! 'patient' %in% names(x))   x$patient <- x$subject
+  if(! 'trial' %in% names(x))     x$trial <- 'none'
+  if(! 'seqnames' %in% names(x))  x$seqnames <- x$chromosome
+  if(! 'start' %in% names(x))     x$start <- x$position
+  if(! 'end' %in% names(x))       x$end <- x$position
+  if(! 'posid' %in% names(x))     x$posid <- paste0(x$seqnames, x$strand, x$position)
+  if(! 'siteLabel' %in% names(x)) x$siteLabel <- paste0(x$subject, '_', x$posid)
+  if('chromosome' %in% names(x)) x$chromosome <- NULL
+  
+  x <- arrange(x, desc(estAbund))
+  x <- x[! duplicated(x$posid),]
+  
+  x
+}
+
+expandTimePoints <- function (tps) {
+  d <- tibble::tibble(tp = sub("_", ".", tps))
+  d$n <- 1:nrow(d)
+  d$timePointType <- stringr::str_match(base::toupper(d$tp),"[DMY]")
+  d$timePointType[which(is.na(d$timePointType))] <- "X"
+  
+  d <- dplyr::bind_rows(lapply(split(d, d$timePointType), function(x) {
+    n <- as.numeric(stringr::str_match(x$tp, "[\\d\\.]+")) * ifelse(grepl("\\-", x$tp), -1, 1)
+    
+    if (x$timePointType[1] == "D") {
+      x$timePointMonths <- base::round(n/30.4167, digits = 0)
+      x$timePointDays <- base::round(n, digits = 0)
+    }
+    else if (x$timePointType[1] == "M") {
+      x$timePointMonths <- base::round(n, digits = 0)
+      x$timePointDays <- base::round(n * 30.4167, digits = 0)
+    }
+    else if (x$timePointType[1] == "Y") {
+      x$timePointMonths <- base::round(n * 12, digits = 0)
+      x$timePointDays <- base::round(n * 365, digits = 0)
+    }
+    else {
+      message("Warning - could not determine date unit for: ",
+              paste0(unique(x$timePoint), collapse = ", "))
+      x$timePointMonths <- n
+      x$timePointDays <- n
+    }
+    x
+  }))
+  
+  dplyr::arrange(d, n) %>% dplyr::select(timePointMonths, timePointDays)
+}
+
+
+createIntUCSCTrack <- function(d, abundCuts = c(5, 10, 20), 
+                               posColors = c("#8C9DFF", "#6768E3", "#4234C7", "#1D00AB"),
+                               negColors = c("#FF8C8C", "#E35D5D", "#C72E2E", "#AB0000"),
+                               title = 'intSites', outputFile = 'track.ucsc', visibility = 1, 
+                               position = 'chr7:127471196-127495720', padSite = 0, siteLabel = NA){
+  
+  # Check function inputs.
+  if(length(posColors) != length(negColors)) 
+    stop('The pos and neg color vectors are not the same length.')
+  
+  if(length(abundCuts) != length(posColors) - 1) 
+    stop('The number of aundance cut offs must be one less than the number of provided colors.')
+  
+  if(! all(c('start', 'end', 'strand', 'seqnames', 'estAbund') %in% names(d))) 
+    stop("The expected column names 'start', 'end', 'strand', 'seqnames', 'estAbund' were not found.") 
+  
+  if(is.na(siteLabel) | ! siteLabel %in% names(d)) 
+    stop('The siteLabel parameter is not defined or can not be found in your data.')
+  
+  # Cut the abundance data. Abundance bins will be used to look up color codes.
+  cuts <- cut(d$estAbund, breaks = c(0, abundCuts, Inf), labels = FALSE)
+  
+  # Convert Hex color codes to RGB color codes. 
+  posColors <- apply(grDevices::col2rgb(posColors), 2, paste0, collapse = ',')
+  negColors <- apply(grDevices::col2rgb(negColors), 2, paste0, collapse = ',')
+  
+  # Create data fields needed for track table.
+  d$score <- 0
+  d$color <- ifelse(d$strand == '+', posColors[cuts], negColors[cuts])
+  
+  # Pad the site n NTs to increase visibility.
+  if(padSite > 0){
+    d$start <- floor(d$start - padSite/2)
+    d$end   <- ceiling(d$end + padSite/2)
+  }
+  
+  # Define track header.
+  trackHead <- sprintf("track name='%s' description='%s' itemRgb='On' visibility=%s\nbrowser position %s",
+                       title, title, visibility, position)
+  
+  # Write out track table.
+  write(trackHead, file = outputFile, append = FALSE)
+  write.table(d[, c('seqnames', 'start', 'end', siteLabel, 'score', 'strand', 'start', 'end', 'color')], 
+              sep = '\t', col.names = FALSE, row.names = FALSE, file = outputFile, append = TRUE, quote = FALSE)
+}
